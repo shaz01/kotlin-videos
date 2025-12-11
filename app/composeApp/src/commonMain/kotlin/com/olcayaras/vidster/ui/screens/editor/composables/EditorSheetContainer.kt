2b5a907e4597a2a2d63c23a1f90c94dd6999bec0
olcay <olcayaras@gmail.com>
2025-12-11 19:45:18 +0300

    Remove useless clip

--- app/composeApp/src/commonMain/kotlin/com/olcayaras/vidster/ui/screens/editor/composables/EditorSheetContainer.kt
+++ app/composeApp/src/commonMain/kotlin/com/olcayaras/vidster/ui/screens/editor/composables/EditorSheetContainer.kt
@@ -3,18 +3,15 @@ package com.olcayaras.vidster.ui.screens.editor.composables
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.olcayaras.vidster.ui.theme.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-private val sheetRadius = RectangleShape
 
 @Composable
 fun EditorSheetContainer(
@@ -24,7 +21,7 @@ fun EditorSheetContainer(
 ) {
     Surface(
         color = MaterialTheme.colorScheme.surfaceContainerHigh.copy(alpha = 0.95f),
-        modifier = modifier.clip(sheetRadius)
+        modifier = modifier
     ) {
         Column(modifier = Modifier.padding(contentPadding)) {
             content()
