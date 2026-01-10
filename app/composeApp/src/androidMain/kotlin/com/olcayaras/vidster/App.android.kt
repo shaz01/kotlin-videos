5463aef38e52e5b8507a041bd7ca93cb3c02f203
olcay <olcayaras@gmail.com>
2026-01-10 22:05:33 +0300

    hide system bars on android

--- app/composeApp/src/androidMain/kotlin/com/olcayaras/vidster/App.android.kt
+++ app/composeApp/src/androidMain/kotlin/com/olcayaras/vidster/App.android.kt
@@ -6,12 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.olcayaras.vidster.ui.App
 
 class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            hide(WindowInsetsCompat.Type.systemBars())
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
         setContent { App() }
     }
 }
